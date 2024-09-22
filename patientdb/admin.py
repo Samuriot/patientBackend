71521050462ca9ee4d7d59f50555c69348273aac
Samuriot <92127962+Samuriot@users.noreply.github.com>
2024-09-22 14:33:05 -0700

    added patients view page for viewing patients details

--- patientdb/admin.py
+++ patientdb/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+# Register your models here for use in the admin panel
+admin.site.register(Patient)
--- patientdb/views.py
+++ patientdb/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import Patient
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello world!")
+    return render(request, "home.html")
+
+def patients(request):
+    items = Patient.objects.all()
+    return render(request, "patients.html", {"patients": items})
