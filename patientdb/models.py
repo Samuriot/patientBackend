b58686046a2e2e0d338809ac6c70847ec036c59a
Samuriot <92127962+Samuriot@users.noreply.github.com>
2024-09-22 13:55:02 -0700

    added patient table to models.py and configured url pathing

--- patientdb/models.py
+++ patientdb/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Patient(models.Model):
-    name = models.CharField(max_length=80)
-    age = models.IntegerField
+    name = models.CharField(max_length=80, default = "")
+    age = models.IntegerField(default=0)
+    address = models.CharField(max_length=200, default = "")
+    insurance_name = models,models.CharField(max_length=80, default = "")
+    insurance_num = models.CharField(max_length=100, default = "")
